42277fa35a753662f2986e15c3b09640ca0e49e7
AgustinDeLeonTorres <agustin.rafael.deleon@hotmail.com>
2025-08-26 20:41:11 -0600

    Initial commit - Spring Boot project setup"

---
+++ gundam-hub-spring/src/main/java/com/agustindlg/gundam_hub_spring/GundamHubSpringApplication.java
@@ -0,0 +1,13 @@
+package com.agustindlg.gundam_hub_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GundamHubSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GundamHubSpringApplication.class, args);
+	}
+
+}
---
+++ gundam-hub-spring/src/test/java/com/agustindlg/gundam_hub_spring/GundamHubSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustindlg.gundam_hub_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GundamHubSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
