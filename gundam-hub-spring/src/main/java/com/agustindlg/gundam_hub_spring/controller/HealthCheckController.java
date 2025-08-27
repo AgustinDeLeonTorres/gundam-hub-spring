ee472d165a89990d3c58dc1e19ee7470d0d2be61
AgustinDeLeonTorres <agustin.rafael.deleon@hotmail.com>
2025-08-27 15:35:30 -0600

    "Added HealthCheckAPI"

---
+++ gundam-hub-spring/src/main/java/com/agustindlg/gundam_hub_spring/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.agustindlg.gundam_hub_spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 1. This marks the class as a REST API controller
+public class HealthCheckController {
+
+    @GetMapping("/health") // 2. This maps HTTP GET requests to this method
+    public String healthCheck() {
+        return "OK Lao Bi! Gundam Hub API is up and running!"; // 3. The response body
+    }
+}
